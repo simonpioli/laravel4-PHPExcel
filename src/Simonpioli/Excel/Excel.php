9c51185b989387128c2c37063e0db3dd75b7baa7
Simon Pioli <simon.pioli@blue-leaf.co.uk>
2014-05-08 23:04:03 +0100

    Variable change

--- src/Simonpioli/Excel/Excel.php
+++ src/Simonpioli/Excel/Excel.php
@@ -178,7 +178,7 @@ class Excel extends \PHPExcel
         }
 
         // Initiate cache
-        $cacheMethod = PHPExcel_CachedObjectStorageFactory::{$cacheType};
+        $cacheMethod = PHPExcel_CachedObjectStorageFactory::$cacheType;
         PHPExcel_Settings::setCacheStorageMethod($cacheMethod, $cacheSettings);
 
     }
